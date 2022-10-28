beef1d3c5a9825809dee1adc0fc9bfaa8fdc88fb
WaltTersly <terslykifugo96@gmail.com>
2022-10-28 11:30:41 +0300

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,6 @@ def teardown_appcontext(error):
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', default='5000'))
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
